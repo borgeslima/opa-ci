8eacc1e05c7a3fbb77703184a82e10d7ffafc579
borgesarch <gabrielborges.web@gmail.com>
2022-09-13 12:22:33 -0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,8 +1,8 @@
 
 
-result=`./opa eval --format pretty --fail-defined --input contract.json --data contract.rego 'data.openapi.police.erros'`
+declare result=`./opa eval --format pretty --fail-defined --input contract.json --data contract.rego 'data.openapi.police.erros'`
 
 if [ "$result" != "[]" ]; then
      echo "One or more policies are violated: $result"
-     
+     exit 1
 fi
